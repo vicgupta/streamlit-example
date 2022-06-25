f8151483ffbac01504c68db88cf7179a8c8e4e3f
Vic Gupta <vicgupta@gmail.com>
2022-06-25 17:03:18 -0400

    Update streamlit_app.py
    m6

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
+import requests
+
 #https://dl.airtable.com/.attachmentThumbnails/b4cea114fb3157dfb0affbd1bac9f0c9/fc94c3db
 im1 = 'https://dl.airtable.com/.attachmentThumbnails/49ab1ed32adabaa49408b3fd81df324c/bd597b11'
 im1 = "https://images.pexels.com/photos/10757791/pexels-photo-10757791.jpeg"
 
 post_text = st.text_input ("Enter Post Text: ")
 
-im = Image.open(im1)
+im = Image.open(requests.get(im1, stream=True).raw)
