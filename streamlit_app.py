db063c673b3f4580895ad2e4405825fe3d3b49e3
Vic Gupta <vicgupta@gmail.com>
2022-06-25 16:44:07 -0400

    Update streamlit_app.py
    m2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from PIL import Image, ImageDraw, ImageFont
+
 
 post_text = st.text_input ("Enter Post Text: ")
+
+
+#font = ImageFont.truetype("Tests/fonts/NotoSans-Regular.ttf", 48)
+im = Image.new("RGB", (200, 200), "white")
+d = ImageDraw.Draw(im)
+d.line(((0, 100), (200, 100)), "gray")
+d.line(((100, 0), (100, 200)), "gray")
+d.text((100, 100), "Quick Brown", fill="black", anchor="ms", font=font)
